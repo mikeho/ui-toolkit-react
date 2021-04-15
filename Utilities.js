7f541f91579a5c5904c8759dd530e58e35a8e922
Mike Ho <mike@quasidea.com>
2021-04-15 15:21:24 -0700

    Added additional utilities

--- Utilities.js
+++ Utilities.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 /**
  * A miscellaneous assortment of methods/utilities
  */
@@ -46,5 +48,80 @@ export default class Utilities {
 
 		// Go
 		reader.readAsDataURL(file);
+	}
+
+	/**
+	 * This will ALWAYS return an array, but it may be blank
+	 *
+	 * This will preform TRIM on all lines of the string, rejecting anything of 0 length
+	 *
+	 * @param {string} text
+	 * @return {string[]}
+	 */
+	static getLineArray(text) {
+		const returnArray = [];
+
+		text.trim().split("\n").forEach(line => {
+			if (line.trim().length) {
+				returnArray.push(line.trim());
+			}
+		});
+
+		return returnArray;
+	}
+
+	/**
+	 * Given a standard string with linebreaks, this will return React Elements
+	 * that can be rendered which will include those same linebreaks.
+	 *
+	 * @param {string} text
+	 * @return {React.Fragment[]}
+	 */
+	static renderTextBlock(text) {
+		const lineArray = [];
+
+		text.trim().split("\n").forEach(line => {
+			lineArray.push(line.trim());
+		});
+
+		return lineArray.map((value, index) => {
+			if (index !== (lineArray.length - 1)) {
+				return (
+					<React.Fragment key={"text-block-" + index}>
+						{value}
+						<br/>
+					</React.Fragment>
+				);
+			} else {
+				return (
+					<React.Fragment key={"text-block-" + index}>
+						{value}
+					</React.Fragment>
+				);
+			}
+		});
+	}
+
+	/**
+	 * Given any partial phone text, it will parse to xxx-xxx-xxxx (or a partial version of it)
+	 * @param {string} text
+	 * @return {string}
+	 */
+	static parsePhone(text) {
+		text = text.replace(/\D/g, '');
+
+		if (text.length > 10) {
+			text = text.substring(0, 10);
+		}
+
+		if (text.length >= 6) {
+			return text.replace(/^(\d{3})(\d{3})/, "$1-$2-");
+		}
+
+		if (text.length >= 3) {
+			return text.replace(/^(\d{3})/, "$1-");
+		}
+
+		return text;
 	}
 }
